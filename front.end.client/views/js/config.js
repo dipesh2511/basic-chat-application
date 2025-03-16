2c8f3b4730691ea1ce0d4e374aefbd1503690e46
dipesh pohanekar <dipeshpohanekar1997@gmail.com>
2025-03-17 05:14:19 +0530

    fix: update config variables to use dynamic URLs based on deployed domain

--- front.end.client/views/js/config.js
+++ front.end.client/views/js/config.js
@@ -1,19 +1,12 @@
-export const config_variables ={
-    BASE_URL: 'http://localhost:3000',
-    API_URL: 'http://localhost:3000/api',
-    INFO_URL: 'http://localhost:3000/info.html',  
-    LOGIN_URL: 'http://localhost:3000/index.html',
-    REGISTER_URL: 'http://localhost:3000/register.html',
-    CHAT_URL: 'http://localhost:3000/chat.html',
-    USER_URL: 'http://localhost:3000/api/user',
-    USER_CREATE: 'http://localhost:3000/api/user',
-    USER_LIST: 'http://localhost:3000/api/user',
-    USER_READ: 'http://localhost:3000/api/user',
-    USER_UPDATE: 'http://localhost:3000/api/user',
-    USER_DELETE: 'http://localhost:3000/api/user',
-    USER_GENERATE_OTP: 'http://localhost:3000/api/user/generate-otp',
-    USER_VERIFY_OTP: 'http://localhost:3000/api/user/verify-otp',
-    USER_LOGIN: 'http://localhost:3000/api/user/login',
-    USER_REGISTER: 'http://localhost:3000/api/user',
-    CREATE_CHAT_URI: 'http://localhost:3000/api/chat',
-}
+export const config_variables = {
+    BASE_URL: window.location.origin, // Dynamically get the deployed domain
+    API_URL: `${window.location.origin}/api`,
+    INFO_URL: `${window.location.origin}/info.html`,
+    LOGIN_URL: `${window.location.origin}/index.html`,
+    REGISTER_URL: `${window.location.origin}/register.html`,
+    CHAT_URL: `${window.location.origin}/chat.html`,
+    USER_URL: `${window.location.origin}/api/user`,
+    USER_LOGIN: `${window.location.origin}/api/user/login`,
+    USER_REGISTER: `${window.location.origin}/api/user`,
+    CREATE_CHAT_URI: `${window.location.origin}/api/chat`,
+};
